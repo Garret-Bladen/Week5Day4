9d36abcc000f27a5740d00898c3761735d789b5b
Garret-Bladen <garretbladen@gmail.com>
2020-09-10 20:56:28 -0600

    added week5day4project

---
+++ Week5Day4Project/app.js
@@ -0,0 +1,7 @@
+$('.navItem').css('color', 'white');
+$('#nav').css('background-color', '#145');
+let evenli = $('li').even();
+$(evenli).css('background-color', '#ccc');
+let oddli = $('li').odd();
+$(oddli).css('background-color', '#aaa');
+$('input').val('Garret')
